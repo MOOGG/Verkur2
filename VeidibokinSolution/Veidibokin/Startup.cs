a860f68aa502c828c3bb9d2caa9194ce58ef87e6
Gudmundur Rafn Gylfason <gvemmsi@gmail.com>
2015-05-02 14:51:59 +0000

    Added VS project files

---
+++ VeidibokinSolution/Veidibokin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Veidibokin.Startup))]
+namespace Veidibokin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
