cb24375b4dc500db4464d8bbac36fd27b41578cc
Oddur Carl Thorarensen <oddurc@gmail.com>
2015-05-05 13:22:24 +0000

    Typpi

--- VeidibokinSolution/Veidibokin/Models/IdentityModels.cs
+++ VeidibokinSolution/Veidibokin/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Data;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,14 @@ namespace Veidibokin.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string fullName { get; set; }
+        public int postalCode { get; set; }
+        public string town { get; set; }
+        public int photoID { get; set; }
+        public char gender { get; set; }
+        //public System.DateTime birthday { get; set; } ákveðið að sleppa þessum upplýsingum að sinni
+        public string info { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -20,6 +29,8 @@ namespace Veidibokin.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<UserStatus> UserStatuses { get; set; }
+        
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
