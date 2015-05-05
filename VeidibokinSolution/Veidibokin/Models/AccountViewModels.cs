8e9bb0936a5dd02df553aae0c08e1a5ae1db1f60
Gudmundur Rafn Gylfason <gvemmsi@gmail.com>
2015-05-05 14:10:20 +0000

    Nyskraning

--- VeidibokinSolution/Veidibokin/Models/AccountViewModels.cs
+++ VeidibokinSolution/Veidibokin/Models/AccountViewModels.cs
@@ -64,19 +64,24 @@ namespace Veidibokin.Models
 
     public class RegisterViewModel
     {
+		[Required]
+		//[EmailAddress]
+		[Display(Name = "Fullt nafn")]
+		public string fullName { get; set; }
+
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "Netfang")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lykilorð")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Lykilorð aftur")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
