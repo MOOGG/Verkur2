25fd582581f0bd8927142392152f7cd55c3b5a87
Oddur Carl Thorarensen <oddurc@gmail.com>
2015-05-05 13:34:57 +0000

    Mamma3

---
+++ VeidibokinSolution/Veidibokin/Models/UserStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Veidibokin.Models
+{
+    public class UserStatus
+    {
+        public int ID { get; set; }
+        public System.DateTime dateInserted { get; set; }
+
+    }
+}
