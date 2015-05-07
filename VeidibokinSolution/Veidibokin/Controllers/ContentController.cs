fed00364375968e676e904fd8641cdcb8c6abb62
Gudmundur Rafn Gylfason <gvemmsi@gmail.com>
2015-05-07 10:39:10 +0000

    Status virkni

--- VeidibokinSolution/Veidibokin/Controllers/ContentController.cs
+++ VeidibokinSolution/Veidibokin/Controllers/ContentController.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Veidibokin.Models;
 
 namespace Veidibokin.Controllers
 {
-	public class ContentController
+	public class ContentController : Controller
 	{
+		public ActionResult NewUserStatus(int userid, FormCollection collection)
+		{
+			string status = collection["userStatusID"];
+			string statusText = collection["StatusText"];
+
+			if (String.IsNullOrEmpty(status))
+			{
+				return View("Error");
+			}
+			if (String.IsNullOrEmpty(statusText))
+			{
+				return RedirectToAction("Index", "Content", new { id = status });
+			}
+
+			string username = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+			int id = Int32.Parse(status);
+			UserStatus user = UserStatus.Instance.GetMovieById(username, id);
+			if (user != null)
+			{
+				UserStatus userStatus = new NewUserStatus { Text = statusText, Username = username, MovieId = id };
+				MovieAppRepository.Instance.AddReview(userStatus);
+				return RedirectToAction("Detail", "MovieApp", new { id = status });
+			}
+			return View("Error");
+		}
 	}
 }
