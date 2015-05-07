f34f66432ac45790a893d524cc37ca9d96e55eeb
olikari <olikari87@gmail.com>
2015-05-07 21:50:57 +0000

    Status Repo

---
+++ VeidibokinSolution/Veidibokin/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Veidibokin.Models;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Windows.Markup;
+using Microsoft.AspNet.Identity;
+
+namespace Veidibokin.Repositories
+{
+    public class UserRepository<Tag> : ParentRepository<Tag> where Tag : class 
+    {
+        protected DbSet<Tag> DbSet;
+
+        public UserRepository(DbContext dataContext)
+        {
+            DbSet = dataContext.Set<Tag>();
+        }
+
+        public IQueryable<Tag> GetAll()
+        {
+            return DbSet;
+        }
+
+        public Tag GetById(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public virtual void Insert(Tag entity)
+        {
+            DbSet.Add(entity);           
+        }
+
+        public void Delete(Tag entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+    }
+}
---
+++ VeidibokinSolution/Veidibokin/Repositories/UserRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Veidibokin.Models;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
+
+namespace Veidibokin.Repositories
+{
+    public class UserRepositoryTest
+    {
+
+        public void QueryUserName()
+        {
+            using (var dataContext = new ApplicationDbContext())
+            {
+                var userStatusRepository = new UserRepository<UserStatus>(dataContext);
+
+                UserStatus newStatus = new UserStatus();
+
+                newStatus.statusText = "TYPPI";
+                newStatus.userId = "83fcec4c-7340-4f13-8464-09237ffe5aa2";
+                newStatus.dateInserted = DateTime.Now;
+                newStatus.isPublic = true;
+                userStatusRepository.Insert(newStatus);
+
+                //Debug.WriteLine(userStatusRepository.GetAll());
+                dataContext.SaveChanges();
+            }
+        }
+    }
+}
