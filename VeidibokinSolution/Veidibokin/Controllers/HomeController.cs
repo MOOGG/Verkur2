455b1c0c964ccc58c46e95e841e848e792321e03
olikari <olikari87@gmail.com>
2015-05-07 21:50:18 +0000

    Status Repo

--- VeidibokinSolution/Veidibokin/Controllers/HomeController.cs
+++ VeidibokinSolution/Veidibokin/Controllers/HomeController.cs
@@ -1,22 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Veidibokin.Models;
 using Veidibokin.Repositories;
 
 namespace Veidibokin.Controllers
 {
 	public class HomeController : Controller
 	{
-		[Authorize]
+        [AllowAnonymous]
 		public ActionResult Index()
 		{
-            UserRepositoryTest myTest = new UserRepositoryTest();
-            myTest.QueryUserName();
 			return View();
-
 		}
+
+        // er ég kannski ekki að senda rétt á milli frá formi í Index til controllers ?
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddStatus(UserStatus status)
+        {
+            // tek inn statusText frá ViewModel-i
+
+            // þarf ég að gera fall sem gerir allt það sem er hér að neðan eins og í UserRepoTest... ???
+
+            // bý til statusinn sem ég ætla að adda
+            var newStatus = new UserStatus()
+            {
+                statusText = status.statusText,
+                isPublic = true,
+                dateInserted = DateTime.Now,
+                userId = User.Identity.GetUserId()
+            };
+
+            var dataContext = new ApplicationDbContext();    
+            var userStatusRepo = new UserRepository<UserStatus>(dataContext);
+            userStatusRepo.Insert(newStatus);
+            dataContext.SaveChanges();
+
+            //insertStatus(model);
+
+            // hvaða view-i á ég að skila hér ???
+            return RedirectToAction("Index", "Home");
+            //return View("Index");
+        }
 
 		public ActionResult About()
 		{
@@ -32,6 +64,21 @@ namespace Veidibokin.Controllers
 			return View();
 		}
 
+	    public void insertStatus(UserStatus status)
+	    {
+	        using (var dataContext = new ApplicationDbContext())
+	        {
+                var newStatus = new UserStatus()
+                {
+                    statusText = status.statusText,
+                    isPublic = true,
+                    dateInserted = DateTime.Now,
+                    userId = User.Identity.GetUserId()
+                };
 
+	            dataContext.UserStatuses.Add(newStatus);
+	            dataContext.SaveChanges();
+	        }
+	    }
 	}
 }
