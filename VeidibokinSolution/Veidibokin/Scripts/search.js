458bacffa14a53b94305dcff530fdb9c01705a5c
Gudmundur Rafn Gylfason <gvemmsi@gmail.com>
2015-05-12 13:47:36 +0000

    Ajax

--- VeidibokinSolution/Veidibokin/Scripts/search.js
+++ VeidibokinSolution/Veidibokin/Scripts/search.js
@@ -1,1 +1,49 @@
-﻿
+﻿$(document).ready(function () {
+    $(function () {
+
+        $("#searchform").on('submit', 'form', function () {
+
+            var theForm = $(this);
+            $("#waitingMessage").show();
+            $.ajax({
+                type: 'POST',
+                url: theForm.attr('action'),
+                data: theForm.serialize(),
+            }).done(function (result) {
+                $("#waitingMessage").hide();
+
+                var resultHtml = $(result).find('#results');
+
+                $('#results').replaceWith(resultHtml);
+
+            }).fail(function () {
+                alert('Villa kom upp!');
+            });
+
+            return false;
+        });
+
+        $("#statusformdrasl").on('submit', 'form', function () {
+
+            var theForm = $(this);
+
+            $("#waitingMessage").show();
+            $.ajax({
+                type: 'POST',
+                url: theForm.attr('action'),
+                data: theForm.serialize(),
+            }).done(function (result) {
+                $("#waitingMessage").hide();
+                var resultHtmls = $(result).find('#statusesdrasl');
+
+                $('#statusesdrasl').replaceWith(resultHtmls);
+
+                theForm.find('#UserStatus').val('');
+            }).fail(function () {
+                alert('Villa kom upp!');
+            });
+
+            return false;
+        });
+    });
+});
